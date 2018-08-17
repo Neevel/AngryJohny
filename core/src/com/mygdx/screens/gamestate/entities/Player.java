15b72c7f0abbb7fe0c031dea9dbd198274999f3d
Krustenkäse <sim>
2018-08-17 23:17:48 +0200

    d

--- core/src/com/mygdx/screens/gamestate/entities/Player.java
+++ core/src/com/mygdx/screens/gamestate/entities/Player.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.screens.gamestate.GameListener;
 
-public class Player extends Entity { // hallo  hier ist eine gans
+public class Player extends Entity { // hallo  hier ist eine gans // hat die gans auch ein name ? 
 
 	private boolean canJump;
 	private GameListener input;
